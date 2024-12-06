5c0cb25be5afaca7d58d7cb161a2ddfbfff9bd60
tilwa <shahrukhaltaf123@gmail.com>
2024-12-06 12:34:07 +0500

    react quiz

---
+++ src/components/Progress.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Progress = ({
+  index,
+  numQuestion,
+  maxPossiblePoints,
+  points,
+  answer,
+}) => {
+  return (
+    <header className="progress">
+      <progress max={numQuestion} value={index + Number(answer !== null)} />
+      <p>
+        Question <strong>{index + 1}</strong> / {numQuestion}
+      </p>
+
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
+};
+
+export default Progress;
